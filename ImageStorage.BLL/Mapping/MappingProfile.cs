23dcd455dbefc24bfed1cce98cfe95638b439535
Staryy-Dobryy <lfcega1@gmail.com>
2023-09-26 22:44:13 +0300

    Added BLL to project.

---
+++ ImageStorage.BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageStorage.BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+    }
+}
