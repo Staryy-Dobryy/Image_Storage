22a0f04a63de1532025a4f72501cae382f24ecf8
Staryy-Dobryy <lfcega1@gmail.com>
2023-09-30 23:40:51 +0300

    Created mapping profile with mapping configuration

--- ImageStorage.BLL/Mapping/MappingProfile.cs
+++ ImageStorage.BLL/Mapping/MappingProfile.cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using ImageStorage.BLL.Models;
+using ImageStorage.BLL.Models.CreateModels;
+using ImageStorage.BLL.Models.UpdateModels;
+using ImageStorage.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,54 @@ namespace ImageStorage.BLL.Mapping
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Account, AccountModel>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            CreateMap<CreateAccountModel, Account>()
+                .ConvertUsing<CreateAccountWithUserConverter>();
+
+            CreateMap<Comment, CommentModel>()
+                .ConvertUsing<CommentModelWithUserConverter>();
+
+            CreateMap<CreateCommentModel, Comment>()
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text))
+                .ForMember(dest => dest.PublicationId, opt => opt.MapFrom(src => src.PublicationId));
+
+            CreateMap<Publication, PublicationModel>()
+                .ConvertUsing<PublicationModelConverter>();
+
+            CreateMap<CreatePublicationModel, Publication>()
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.IsPublic, opt => opt.MapFrom(src => src.IsPublic));
+
+            CreateMap<UpdatePublicationModel, Publication>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.IsPublic, opt => opt.MapFrom(src => src.IsPublic));
+
+            CreateMap<GoogleAuthModel, User>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.GoogleAuth, opt => opt.MapFrom(src => true));
+
+            CreateMap<User, UserModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+
+            CreateMap<User, JwtUserModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+
+            CreateMap<UpdateUserModel, User>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl));
+        }
     }
 }
