89f7f92ed1595f2c13050eb3c1f4433b1eb238b4
Staryy-Dobryy <lfcega1@gmail.com>
2023-10-03 23:23:09 +0300

    Created service for gallery page.

---
+++ angularapp/src/app/Services/gallery.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { IGallery } from "../Models/gallery.model";
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class GalleryService {
+  constructor(private http: HttpClient) {
+  }
+
+  getGeneral(): Observable<IGallery> {
+    return this.http.get<IGallery>("/general");
+  }
+}
