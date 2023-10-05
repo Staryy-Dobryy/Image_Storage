c26477a4a284cbb5f4ec97f859ac9890d8de8747
Staryy-Dobryy <lfcega1@gmail.com>
2023-10-05 23:50:51 +0300

    Models updated. Added validations attributes.

--- ImageStorage.BLL/Models/PublicationModel.cs
+++ ImageStorage.BLL/Models/PublicationModel.cs
@@ -11,6 +11,7 @@ namespace ImageStorage.BLL.Models
         public string Id { get; set; }
         public string Discription { get; set; }
         public string ImageUrl { get; set; }
+        public int Views { get; set; }
         public UserModel Author { get; set; }
         public List<CommentModel> Comments { get; set; }
     }
