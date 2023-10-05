0ad737521aa4416064fc64e1859059fadab73bbf
Staryy-Dobryy <lfcega1@gmail.com>
2023-10-05 23:46:00 +0300

    Created middlewate for catching exceptions

---
+++ webapi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using ImageStorage.BLL.Exceptions;
+using ImageStorage.BLL.Exeptions;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next.Invoke(context);
+        }
+        catch (Exception ex)
+        {
+            switch (ex.GetType())
+            {
+                case Type exType when 
+                exType == typeof(AccountAlreadyExistsException) ||
+                exType == typeof(AccountDoesNotExistException) ||
+                exType == typeof(AccountLoginFailedException) ||
+                exType == typeof(AccountUsesGoogleAuthException):
+
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync(ex.Message);
+
+                    _logger.Log(LogLevel.Error, 1, exType + ex.Message);
+                    break;
+
+                case Type exType when exType == typeof(NullReferenceException):
+                    context.Response.StatusCode = 400;
+                    _logger.Log(LogLevel.Critical, 1, exType + ex.Message);
+                    break;
+
+                default:
+                    _logger.Log(LogLevel.Critical, ex.Message);
+                    break;
+            }
+        }
+    }
+}
