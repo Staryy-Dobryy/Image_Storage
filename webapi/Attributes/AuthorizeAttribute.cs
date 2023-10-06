b0ef8f670781d272e8c35773d54c243ba8c4de87
Staryy-Dobryy <lfcega1@gmail.com>
2023-10-06 23:42:34 +0300

    Jwt authentication ipmlement

---
+++ webapi/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using ImageStorage.BLL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ImageStorage.Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var jwtUserModel = (JwtUserModel)context.HttpContext.Items["jwtUserModel"];
+            if (jwtUserModel is null)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
---
+++ webapi/Middlewares/JwtMiddleware.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageStorage.BLL.Models;
+using ImageStorage.BLL.Services.Interfaces;
+using ImageStorage.BLL.Services.Realization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Sailora.Identity.Helpers
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+                AttachUserToContext(context, token);
+
+            await _next(context);
+        }
+
+        public void AttachUserToContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_configuration["Secret"]);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = Guid.Parse(jwtToken.Claims.First(x => x.Type == "Id").Value);
+                var userEmail = jwtToken.Claims.First(x => x.Type == "Email").Value;
+
+                JwtUserModel jwtUserModel = new()
+                {
+                    Id = userId,
+                    Email = userEmail
+                };
+
+                context.Items["jwtUserModel"] = jwtUserModel;
+            }
+            catch
+            {
+                // todo: need to add logger
+            }
+        }
+    }
+}
