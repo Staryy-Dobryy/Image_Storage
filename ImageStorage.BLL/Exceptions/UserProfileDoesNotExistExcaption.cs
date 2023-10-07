b6d8dae79b930661c6719e5a52b6f30aa099951a
Staryy-Dobryy <lfcega1@gmail.com>
2023-10-07 21:24:22 +0300

    Added 'UserProfileDoesNotExtis' exception for throwing.

---
+++ ImageStorage.BLL/Exceptions/UserProfileDoesNotExistExcaption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageStorage.BLL.Exceptions
+{
+    public class UserProfileDoesNotExistExcaption : Exception
+    {
+        public UserProfileDoesNotExistExcaption() : base("This user profile does not exist") { }
+    }
+}
